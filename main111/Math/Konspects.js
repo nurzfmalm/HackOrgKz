ef5afbc8bc3da52305a9260ac67a11628f8b099b
nurzfmalm <nurzatkassymov@gmail.com>
2024-06-22 21:24:10 +0500

    21:24

--- main111/Math/Konspects.js
+++ main111/Math/Konspects.js
@@ -1,20 +1,33 @@
-function Question(){
-    var Count = 0;
-    if(document.getElementById("first").checked){
-        Count+=1;
-    }
-    if(document.getElementById("Dva")){
-        Count+=1;
-    }
-    if(document.getElementById("Tri")){
-        Count+=1;
-    }
-    alert("Ты получил "+ Count + " NIS coins");
-    window.location.href = "Math.html";
-}
 function Menu(){
     window.location.href = "../mainHTT.html";
 }
-function ToTest(){
-    window.location.href = "MathTest.html";
+function SAT() {
+    window.location.href = "./SAT konspects/SAT.html";
+}
+function Back(){
+    window.history.back();
+}
+function Math(){
+    window.location.href = "./Math.html";
+}
+function Reading(){
+    window.location.href = "./Reading.html";
+}
+function Writing(){
+    window.location.href = "./Writing.html";
+}
+function IELTS(){
+    window.location.href = "./IELTS konspects/IELTS.html";
+}
+function ReadingI(){
+    window.location.href = "./Reading.html";
+}
+function WritingI(){
+    window.location.href = "./Writing.html";
+}
+function SpeakingI(){
+    window.location.href = "./Speaking.html";
+}
+function ListeningI(){
+    window.location.href = "./Listening.html";
 }
